8cc9dc0ed5f7626d6fef6466a53ac10f883880d5
Gaberu <33635431+gaberu13@users.noreply.github.com>
2019-06-11 03:05:49 +0200

    Editing some HTML,CSS details, Translating to Macedonian, Converting measuring units

--- app/app.js
+++ app/app.js
@@ -4,7 +4,6 @@
 angular.module('myApp', [])
 .controller('weatherCon',function ($scope,$http) {
     var vm = $scope;
-    vm.tester = "Home";
     $http({
         method:'GET',
         mode:'json',
@@ -17,12 +16,41 @@ angular.module('myApp', [])
     $http({
         method: 'GET',
         mode:'json',
-        url:'http://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=c9bc3074b0f956b8e2dab195b09098c7'
+        url:'http://api.openweathermap.org/data/2.5/forecast?q=Berlin,ger&appid=c9bc3074b0f956b8e2dab195b09098c7'
     }).then(function (data) {
         vm.description= data.data.list[0].weather.description;
         vm.city=data.data.city.name;
+        vm.vetar = data.data.list[0].wind.speed;
+
+        vm.slika=data.data.list[0].weather[0].icon;
+        vm.slika1=data.data.list[1].weather[0].icon;
+        vm.slika2=data.data.list[2].weather[0].icon;
+        vm.slika3=data.data.list[3].weather[0].icon;
+        vm.slika4=data.data.list[4].weather[0].icon;
+        vm.slika5=data.data.list[5].weather[0].icon;
+        vm.slika6=data.data.list[6].weather[0].icon;
+
         vm.vreme=timeConverter(data.data.list[0].dt);
+        vm.vreme1=timeConverter(data.data.list[1].dt);
+        vm.vreme2=timeConverter(data.data.list[2].dt);
+        vm.vreme3=timeConverter(data.data.list[3].dt);
+        vm.vreme4=timeConverter(data.data.list[4].dt);
+        vm.vreme5=timeConverter(data.data.list[5].dt);
+        vm.vreme6=timeConverter(data.data.list[6].dt);
+
         vm.stepeniDenes = convertStepeni(data.data.list[0].main.temp);
+        vm.stepeniDenes1 = convertStepeni(data.data.list[1].main.temp);
+        vm.stepeniDenes2 = convertStepeni(data.data.list[2].main.temp);
+        vm.stepeniDenes3 = convertStepeni(data.data.list[3].main.temp);
+        vm.stepeniDenes4 = convertStepeni(data.data.list[4].main.temp);
+        vm.stepeniDenes5 = convertStepeni(data.data.list[5].main.temp);
+        vm.stepeniDenes6 = convertStepeni(data.data.list[6].main.temp);
+
+
+
+
+
+        vm.vlaznostDenes = data.data.list[0].main.humidity;
 
 
 
@@ -48,7 +76,7 @@ function timeConverter(UNIX_timestamp){
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year  ;
+    var time = date + ' ' + month + ' ' + year + ' ' + hour+':00';
     return time;
 }
 function convertStepeni(cudenBroj){
