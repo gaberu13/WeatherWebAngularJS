ae4ccbcdc8a9cee55c6e919c50d2a9451ba1aef4
Gaberu <33635431+gaberu13@users.noreply.github.com>
2019-06-11 13:03:51 +0200

    Adding functionality on Serach bar, adding list of cities with autocomplete and suggested cities

--- app/app.js
+++ app/app.js
@@ -2,70 +2,91 @@
 
 // Declare app level module which depends on views, and core components
 angular.module('myApp', [])
-.controller('weatherCon',function ($scope,$http) {
-    var vm = $scope;
-    $http({
-        method:'GET',
-        mode:'json',
-        url:'http://ip-api.com/json'
-    }).then(function (data) {
-        vm.late =data.data.lat ;
-        vm.lone= data.data.lon;
-
-    });
-    $http({
-        method: 'GET',
-        mode:'json',
-        url:'http://api.openweathermap.org/data/2.5/forecast?q=Berlin,ger&appid=c9bc3074b0f956b8e2dab195b09098c7'
-    }).then(function (data) {
-        vm.description= data.data.list[0].weather.description;
-        vm.city=data.data.city.name;
-        vm.vetar = data.data.list[0].wind.speed;
-
-        vm.slika=data.data.list[0].weather[0].icon;
-        vm.slika1=data.data.list[1].weather[0].icon;
-        vm.slika2=data.data.list[2].weather[0].icon;
-        vm.slika3=data.data.list[3].weather[0].icon;
-        vm.slika4=data.data.list[4].weather[0].icon;
-        vm.slika5=data.data.list[5].weather[0].icon;
-        vm.slika6=data.data.list[6].weather[0].icon;
-
-        vm.vreme=timeConverter(data.data.list[0].dt);
-        vm.vreme1=timeConverter(data.data.list[1].dt);
-        vm.vreme2=timeConverter(data.data.list[2].dt);
-        vm.vreme3=timeConverter(data.data.list[3].dt);
-        vm.vreme4=timeConverter(data.data.list[4].dt);
-        vm.vreme5=timeConverter(data.data.list[5].dt);
-        vm.vreme6=timeConverter(data.data.list[6].dt);
-
-        vm.stepeniDenes = convertStepeni(data.data.list[0].main.temp);
-        vm.stepeniDenes1 = convertStepeni(data.data.list[1].main.temp);
-        vm.stepeniDenes2 = convertStepeni(data.data.list[2].main.temp);
-        vm.stepeniDenes3 = convertStepeni(data.data.list[3].main.temp);
-        vm.stepeniDenes4 = convertStepeni(data.data.list[4].main.temp);
-        vm.stepeniDenes5 = convertStepeni(data.data.list[5].main.temp);
-        vm.stepeniDenes6 = convertStepeni(data.data.list[6].main.temp);
+.controller('weatherCon',povik);
+    function povik($scope,$http) {
+        var vm = $scope;
+        vm.getCity = "";
+        vm.cities ="Tirana Yerevan Vienna Baku Minsk Brussels Sarajevo Sofia Zagreb Nicosia Prague Copenhagen Tallinn Helsinki Paris Tbilisi Berlin Athens Budapest Reykjavik Dublin Rome Nur-Sultan Pristina Riga Vaduz Vilnius Luxembourg Valletta Chisinau Monaco Podgorica Amsterdam Skopje Oslo Warsaw Lisbon Bucharest Moscow San Marino Belgrade Bratislava Ljubljana Madrid Stockholm Bern Ankara Kyiv London".split(' ');
 
 
+        $http({
+            method: 'GET',
+            mode:'json',
+            url:'http://api.openweathermap.org/data/2.5/forecast?q=Skopje&appid=c9bc3074b0f956b8e2dab195b09098c7'
+        }).then(function (data){
+
+            vm.city=data.data.city.name;
+
+            vm.vetar = data.data.list[0].wind.speed;
+
+            vm.slika = data.data.list[0].weather[0].icon;
+            vm.slika1 = data.data.list[1].weather[0].icon;
+            vm.slika2 = data.data.list[2].weather[0].icon;
+            vm.slika3 = data.data.list[3].weather[0].icon;
+            vm.slika4 = data.data.list[4].weather[0].icon;
+            vm.slika5 = data.data.list[5].weather[0].icon;
+            vm.slika6 = data.data.list[6].weather[0].icon;
+
+            vm.vreme = timeConverter(data.data.list[0].dt);
+            vm.vreme1 = timeConverter(data.data.list[1].dt);
+            vm.vreme2 = timeConverter(data.data.list[2].dt);
+            vm.vreme3 = timeConverter(data.data.list[3].dt);
+            vm.vreme4 = timeConverter(data.data.list[4].dt);
+            vm.vreme5 = timeConverter(data.data.list[5].dt);
+            vm.vreme6 = timeConverter(data.data.list[6].dt);
+
+            vm.stepeniDenes = convertStepeni(data.data.list[0].main.temp);
+            vm.stepeniDenes1 = convertStepeni(data.data.list[1].main.temp);
+            vm.stepeniDenes2 = convertStepeni(data.data.list[2].main.temp);
+            vm.stepeniDenes3 = convertStepeni(data.data.list[3].main.temp);
+            vm.stepeniDenes4 = convertStepeni(data.data.list[4].main.temp);
+            vm.stepeniDenes5 = convertStepeni(data.data.list[5].main.temp);
+            vm.stepeniDenes6 = convertStepeni(data.data.list[6].main.temp);
 
 
-
-        vm.vlaznostDenes = data.data.list[0].main.humidity;
-
-
-
-
-    },function (error) {
+            vm.vlaznostDenes = data.data.list[0].main.humidity;
 
         });
-    //var apiKey = "c9bc3074b0f956b8e2dab195b09098c7";
-       /* var openWeatherURL="https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=c9bc3074b0f956b8e2dab195b09098c7";
-        $http.get(openWeatherURL).success(function (data) {
-            vm.description = data.weather[0].description;
 
-        });*/
 
-});
+        vm.getResult = function () {
+            $http.get("http://api.openweathermap.org/data/2.5/forecast?q="+vm.getCity+"&appid=c9bc3074b0f956b8e2dab195b09098c7").then(function (data) {
+
+                vm.city=data.data.city.name;
+
+                vm.vetar = data.data.list[0].wind.speed;
+
+                vm.slika = data.data.list[0].weather[0].icon;
+                vm.slika1 = data.data.list[1].weather[0].icon;
+                vm.slika2 = data.data.list[2].weather[0].icon;
+                vm.slika3 = data.data.list[3].weather[0].icon;
+                vm.slika4 = data.data.list[4].weather[0].icon;
+                vm.slika5 = data.data.list[5].weather[0].icon;
+                vm.slika6 = data.data.list[6].weather[0].icon;
+
+                vm.vreme = timeConverter(data.data.list[0].dt);
+                vm.vreme1 = timeConverter(data.data.list[1].dt);
+                vm.vreme2 = timeConverter(data.data.list[2].dt);
+                vm.vreme3 = timeConverter(data.data.list[3].dt);
+                vm.vreme4 = timeConverter(data.data.list[4].dt);
+                vm.vreme5 = timeConverter(data.data.list[5].dt);
+                vm.vreme6 = timeConverter(data.data.list[6].dt);
+
+                vm.stepeniDenes = convertStepeni(data.data.list[0].main.temp);
+                vm.stepeniDenes1 = convertStepeni(data.data.list[1].main.temp);
+                vm.stepeniDenes2 = convertStepeni(data.data.list[2].main.temp);
+                vm.stepeniDenes3 = convertStepeni(data.data.list[3].main.temp);
+                vm.stepeniDenes4 = convertStepeni(data.data.list[4].main.temp);
+                vm.stepeniDenes5 = convertStepeni(data.data.list[5].main.temp);
+                vm.stepeniDenes6 = convertStepeni(data.data.list[6].main.temp);
+
+
+                vm.vlaznostDenes = data.data.list[0].main.humidity;
+
+            });
+        };
+
+}
 
 function timeConverter(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
